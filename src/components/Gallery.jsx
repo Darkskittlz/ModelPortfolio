7f16650d3ffef6afb1383928f92c25341b0a8f5b
Tristan <jneal@breezeline.com>
2023-07-10 10:09:50 -0400

    image changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -26,26 +26,26 @@ export class MyGallery extends React.Component {
     render() {
         return (
             <ImageGridContainer>
-                <img src={modelIMG1} />
-                <img src={modelIMG3} />
-                <img src={modelIMG4} />
-                <img src={modelIMG12} />
-                <img src={modelIMG11} />
-                <img src={modelIMG14} />
-                <img src={modelIMG19} />
-                <img src={modelIMG20} />
-                <img src={modelIMG13} />
-                <img src={modelIMG10} />
-                <img src={modelIMG9} />
-                <img src={modelIMG18} />
-                <img src={modelIMG15} />
-                <img src={modelIMG17} />
-                <img src={modelIMG16} />
-                <img src={modelIMG8} />
-                <img src={modelIMG5} />
-                <img src={modelIMG6} />
-                <img src={modelIMG7} />
-                <img src={modelIMG2} />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(1).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(3).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(4).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(12).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(11).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(14).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(19).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(20).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(13).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(10).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(9).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(18).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(15).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(17).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(16).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(8).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(5).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(6).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(7).jpg?raw=true" />
+                <img src="https://github.com/Darkskittlz/ModelPortfolio/blob/main/src/assets/images/modelIMG%20(2).jpg?raw=true" />
             </ImageGridContainer>
         )
     }
